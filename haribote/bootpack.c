0d45d2320a236a623ac80ee90fbfc6abcddc72eb
yi.huang <yi.huang@inceptioglobal.ai>
2026-09-02 23:24:13 -0700

    day9

--- haribote/bootpack.c
+++ haribote/bootpack.c
@@ -3,23 +3,33 @@
 #include "bootpack.h"
 #include <stdio.h>
 
-extern struct FIFO8 keyfifo, mousefifo;
+#define MEMMAN_FREES 4090 /* 大约是32KB*/
 
-struct MOUSE_DEC {
-	unsigned char buf[3], phase;
-	int x, y, btn;
+struct FREEINFO { /* 可用信息 */
+	unsigned int addr, size;
+};
+struct MEMMAN { /* 内存管理 */
+	int frees, maxfrees, lostsize, losts;
+	struct FREEINFO free[MEMMAN_FREES];
 };
 
-void enable_mouse(struct MOUSE_DEC *mdec);
-int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
-void init_keyboard(void);
+unsigned int memtest(unsigned int start, unsigned int end);
+void memman_init(struct MEMMAN *man);
+unsigned int memman_total(struct MEMMAN *man);
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
+
+#define MEMMAN_ADDR 0x003c0000
 
 void HariMain(void)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
 	char s[40], mcursor[256], keybuf[32], mousebuf[128];
 	int mx, my, i;
-	struct MOUSE_DEC mdec; 
+	struct MOUSE_DEC mdec;
+
+	unsigned int memtotal;
+	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
 
 	init_gdtidt();
 	init_pic();
@@ -31,6 +41,11 @@ void HariMain(void)
 	io_out8(PIC1_IMR, 0xef); /* 开放鼠标中断(11101111) */
 
 	init_keyboard();
+	enable_mouse(&mdec);
+	memtotal = memtest(0x00400000, 0xbfffffff);
+	memman_init(memman);
+	memman_free(memman, 0x00001000, 0x0009e000); /* 0x00001000 - 0x0009efff */
+	memman_free(memman, 0x00400000, memtotal - 0x00400000);
 
 	init_palette();
 	init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
@@ -41,7 +56,8 @@ void HariMain(void)
 	sprintf(s, "(%d, %d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
-	enable_mouse(&mdec);
+	sprintf(s, "memory %dMB free : %dKB", memtotal / (1024 * 1024), memman_total(memman) / 1024);
+	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
 
 	for (;;) {
 		io_cli();
@@ -58,7 +74,7 @@ void HariMain(void)
 				i = fifo8_get(&mousefifo);
 				io_sti();
 				if (mouse_decode(&mdec, i) != 0) {
-					 /* 3字节都凑齐了，所以把它们显示出来*/
+					/* 3字节都凑齐了，所以把它们显示出来*/
 					sprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
 					if ((mdec.btn & 0x01) != 0) {
 						s[1] = 'L';
@@ -97,85 +113,144 @@ void HariMain(void)
 	}
 }
 
-#define PORT_KEYDAT       0x0060
-#define PORT_KEYSTA       0x0064
-#define PORT_KEYCMD       0x0064
-#define KEYSTA_SEND_NOTREADY  0x02
-#define KEYCMD_WRITE_MODE   0x60
-#define KBC_MODE        0x47
+#define EFLAGS_AC_BIT			0x00040000
+#define CR0_CACHE_DISABLE	0x60000000
 
-void wait_KBC_sendready(void)
+unsigned int memtest(unsigned int start, unsigned int end) 
 {
-	/* 等待键盘控制电路准备完毕 */
-	for (;;) {
-		if ((io_in8(PORT_KEYSTA) & KEYSTA_SEND_NOTREADY) == 0) {
+	char flg486 = 0;
+	unsigned int eflg, cr0, i;
+
+	/* 确认CPU是386还是486以上的 */
+	eflg = io_load_eflags();
+	eflg |= EFLAGS_AC_BIT; /* AC-bit = 1 */
+	io_store_eflags(eflg);
+	eflg = io_load_eflags();
+	if ((eflg & EFLAGS_AC_BIT) != 0) {
+		/* 如果是386，即使设定AC=1，AC的值还会自动回到0 */
+		flg486 = 1;
+	}
+
+	eflg &= ~EFLAGS_AC_BIT; /* AC-bit = 0 */
+	io_store_eflags(eflg);
+
+	if (flg486 != 0) {
+		cr0 = load_cr0();
+		cr0 |= CR0_CACHE_DISABLE; /* 禁止缓存 */ 
+		store_cr0(cr0);
+	}
+
+	i = memtest_sub(start, end);
+
+	if (flg486 != 0) {
+		cr0 = load_cr0();
+		cr0 &= ~CR0_CACHE_DISABLE; /* 允许缓存 */
+		store_cr0(cr0);
+	}
+
+	return i;
+}
+
+void memman_init(struct MEMMAN *man){
+	man->frees = 0;    /* 可用信息数目 */
+	man->maxfrees = 0; /* 用于观察可用状况：frees的最大值 */
+	man->lostsize = 0; /* 释放失败的内存的大小总和 */
+	man->losts = 0;    /* 释放失败次数 */
+	return;
+}
+
+unsigned int memman_total(struct MEMMAN *man)
+/* 报告空余内存大小的合计 */
+{
+	unsigned int i, t = 0;
+	for (i = 0; i < man->frees; i++) {
+		t += man->free[i].size;
+	}
+	return t;
+}
+
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size)
+/* 分配 */
+{
+	unsigned int i, a;
+	for (i = 0; i < man->frees; i++) {
+		if (man->free[i].size >= size) {
+			/* 找到了足够大的内存 */
+			a = man->free[i].addr;
+			man->free[i].addr += size;
+			man->free[i].size -= size;
+			if (man->free[i].size == 0) {
+				/* 如果free[i]变成了0，就减掉一条可用信息 */
+				man->frees--;
+				for (; i < man->frees; i++) {
+					man->free[i] = man->free[i + 1]; /* 代入结构体 */
+				}
+			}
+			return a;
+		}
+	}
+	return 0; /* 没有可用空间 */
+}
+
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size)
+/* 释放 */
+{
+	int i, j;
+	/* 为便于归纳内存，将free[]按照addr的顺序排列 */
+	/* 所以，先决定应该放在哪里 */
+	for (i = 0; i < man->frees; i++) {
+		if (man->free[i].addr > addr) {
 			break;
 		}
 	}
-	return;
-}
-
-void init_keyboard(void)
-{
-	/* 初始化键盘控制电路 */
-	wait_KBC_sendready();
-	io_out8(PORT_KEYCMD, KEYCMD_WRITE_MODE);
-	wait_KBC_sendready();
-	io_out8(PORT_KEYDAT, KBC_MODE);
-	return;
-}
-
-#define KEYCMD_SENDTO_MOUSE   0xd4
-#define MOUSECMD_ENABLE     0xf4
-
-void enable_mouse(struct MOUSE_DEC *mdec){
-	/* 鼠标有效 */
-	wait_KBC_sendready();
-	io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
-	wait_KBC_sendready();
-	io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
-	/* 顺利的话，ACK(0xfa)会被送过来 */
-	mdec->phase = 0; /* 等待0xfa的阶段 */    
-	return;
-}
-
-int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat){
-	if (mdec->phase == 0) {
-		/* 等待鼠标的0xfa的阶段 */
-		if (dat == 0xfa) {
-			mdec->phase = 1;
-		}        
-		return 0;
-	}
-	if (mdec->phase == 1) {
-		/* 等待鼠标第一字节的阶段 */
-		mdec->buf[0] = dat;
-		mdec->phase = 2;
-		return 0;
-	}
-	if (mdec->phase == 2) {
-		/* 等待鼠标第二字节的阶段 */
-		mdec->buf[1] = dat;
-		mdec->phase = 3;
-		return 0;
-	}
-	if (mdec->phase == 3) {
-		/* 等待鼠标第二字节的阶段 */
-		mdec->buf[2] = dat;
-		mdec->phase = 1;
-		mdec->btn = mdec->buf[0] & 0x07;
-		mdec->x = mdec->buf[1];
-		mdec->y = mdec->buf[2];
-		if ((mdec->buf[0] & 0x10) != 0) {
-			mdec->x |= 0xffffff00;
+	/* free[i - 1].addr < addr < free[i].addr */
+	if (i > 0) {
+		/* 前面有可用内存 */
+		if (man->free[i - 1].addr + man->free[i - 1].size == addr) {
+			/* 可以与前面的可用内存归纳到一起 */
+			man->free[i - 1].size += size;
+			if (i < man->frees) {
+				/* 后面也有 */
+				if (addr + size == man->free[i].addr) {
+					/* 也可以与后面的可用内存归纳到一起 */
+					man->free[i - 1].size += man->free[i].size;
+					/* man->free[i]删除 */
+					/* free[i]变成0后归纳到前面去 */
+					man->frees--;
+					for (; i < man->frees; i++) {
+						man->free[i] = man->free[i + 1]; /* 结构体赋值 */
+					}
+				}
+			}
+			return 0; /* 成功完成 */
 		}
-		if ((mdec->buf[0] & 0x20) != 0) {
-			mdec->y |= 0xffffff00;
-		}     
-		/* 鼠标的y方向与画面符号相反 */   
-		mdec->y = - mdec->y; 
-		return 1;
 	}
-	/* 应该不可能到这里来 */
-	return -1; 
+	/* 不能与前面的可用空间归纳到一起 */
+	if (i < man->frees) {
+		/* 后面还有 */
+		if (addr + size == man->free[i].addr) {
+			/* 可以与后面的内容归纳到一起 */
+			man->free[i].addr = addr;
+			man->free[i].size += size;
+			return 0; /* 成功完成 */
+		}
+	}
+	/* 既不能与前面归纳到一起，也不能与后面归纳到一起 */
+	if (man->frees < MEMMAN_FREES) {
+		/* free[i]之后的，向后移动，腾出一点可用空间 */
+		for (j = man->frees; j > i; j--) {
+			man->free[j] = man->free[j - 1];
+		}
+		man->frees++;
+		if (man->maxfrees < man->frees) {
+			man->maxfrees = man->frees; /* 更新最大值 */
+		}
+		man->free[i].addr = addr;
+		man->free[i].size = size;
+		return 0; /* 成功完成 */
+	}
+	/* 不能往后移动 */
+	man->losts++;
+	man->lostsize += size;
+	return -1; /* 失败 */
 }
